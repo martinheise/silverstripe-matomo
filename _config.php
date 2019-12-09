cd78f72ce767c018e487d37bf5aa3e5af003719f
Martin Heise <info@martinheise.de>
2019-12-09 12:19:00 +0100

    init empty module

---
+++ _config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright (c) 2019, Martin Heise <info@martinheise.de>
+ * All rights reserved.
+ *
+ * @author     Martin Heise <info@martinheise.de>
+ * @created    09.12.2019
+ */
+
